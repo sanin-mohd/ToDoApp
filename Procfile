15f732ff2df49fead5365067bf8b732bed928ea7
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-10-29 20:39:28 +0530

    heroku hosting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn toDoApp.wsgi --log-file -
--- toDoApp/settings.py
+++ toDoApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bt!=)6yh__sf(v&mbd%1@gts*z#8ptpfwfjhamt4m7jzj=@p#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['to---do--app.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
